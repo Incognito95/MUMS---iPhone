30cfb129669f13c81362e51187aa4edfcb33fdfa
Incognito95 <35996745+Incognito95@users.noreply.github.com>
2018-06-06 23:38:36 +0200

    Add files via upload

---
+++ sweetalert.js
@@ -0,0 +1,4 @@
+var bestil = document.querySelector(".bestil")
+bestil.querySelector("button").addEventListener("click", function() {
+    swal("Tak for din bestilling!", "Håber du kommer tilbage snart!")
+});
